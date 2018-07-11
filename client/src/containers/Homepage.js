619730a96c0de5672e556a78537791d81837eeee
github username <yosa.yon@gmail.com>
2018-07-11 17:23:23 -0400

    added about container

--- client/src/containers/Homepage.js
+++ client/src/containers/Homepage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import background from '../pizza-background-animation.gif'
+import background from '../pizza-background.png'
 
 
 export default class Homepage extends Component {
@@ -7,6 +7,10 @@ export default class Homepage extends Component {
   return(
     <div id='background-container'>
       <img alt='background' className='pizza-background' src={background}/>
+      <div className='overlay'>
+        <div className='one-bite-message'>ONE BITE EVERYBODY KNOWS THE RULES - Dave Portnoy, El Presidente</div>
+        <div className='one-bite-message'>Visit <a href='http://www.barstoolsports.com'>barstoolsports</a></div>
+      </div>
     </div>
   )}
 }
