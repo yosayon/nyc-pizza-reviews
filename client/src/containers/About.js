65f1408d9d6b83e43a2916f6a9a814600a32e69d
github username <yosa.yon@gmail.com>
2018-07-11 17:24:14 -0400

    added small pizza logo in nav and it directs to homepage

---
+++ client/src/containers/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+export const About = () => {
+  return (
+    <div id='about-container'>
+      <p>  I created this website to tribute to Dave Portnoy's New York Pizza reviews. After scavenging the web for a list of places he's reviewed, like many of you I came up empty handed. What ignited during this tragedy was a feeling of determination,  a calling if I'm so inclined. After many hours of video, and many hours of data-entry, I am proud to present to Dave and  his fans a working v1 of Dave Portnoy's New York City Pizza Review Collections list. Why? Because farewell crappy, NYC  pizza, farewell frustrated sighs of indecisive hunger and farewell finger strains from google search.
+      </p>
+
+      <p> So frolickly search to your heart's content whether it be by best-rating or by location etc. In hopes of ensuring the accuracy of the data, please be obliged to send edit requests to yosa.yon@gmail.com.
+      </p>
+      
+    </div>
+  )
+}
