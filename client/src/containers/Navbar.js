fac5f9697c957be2e880a203fe281ebde228338e
github username <yosa.yon@gmail.com>
2018-07-11 17:23:35 -0400

    added path to about section

--- client/src/containers/Navbar.js
+++ client/src/containers/Navbar.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom'
+import logo from '../pizza-logo.jpeg'
 
 export const Navbar = () => {
     return(
       <div id='navbar-container'>
 
          <NavLink className='nav-link' to='/pizza-reviews'> Pizza Reviews </NavLink>
+         <NavLink className='pizza-logo' to='/'><img alt='logo' src={logo}/></NavLink>
+         <NavLink className='nav-link' to='/about'> About </NavLink>
 
       </div>
     )
