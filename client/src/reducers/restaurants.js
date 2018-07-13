d11c195f710c3726ba7f1d6adbd838329ff7acbf
github username <yosa.yon@gmail.com>
2018-07-12 20:49:05 -0400

    replaced FETCH_RESTAURANT with ADD_RESTAURANT and added state.sort and fixed spread operator assignments

--- client/src/reducers/restaurants.js
+++ client/src/reducers/restaurants.js
@@ -1,10 +1,10 @@
-export default function restaurantReducer(state = {all: [], loading: false }, action){
+export default function restaurantReducer(state = {all: [], loading: false, sort: 'all' }, action){
   switch(action.type){
     case 'LOAD_RESTAURANTS':
-      return {all: [], loading: true}
-    case 'FETCH_RESTAURANTS':
-      const restaurants = action.restaurants
-      return {all: restaurants, loading: false}
+    console.log("Loading.....")
+      return {...state, loading: true }
+    case 'ADD_RESTAURANTS':
+      return {...state, all: action.restaurants, loading: false }
     default:
       return state;
   }
