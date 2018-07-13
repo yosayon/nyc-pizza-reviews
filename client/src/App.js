3b189f3fb04faecf42dd9e0b2e31069272657d99
github username <yosa.yon@gmail.com>
2018-07-12 20:45:26 -0400

    no changes here

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
-import { fetchRestaurants } from './actions/restaurantActions';
-import RestaurantList from './components/RestaurantList';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-import Homepage from './containers/Homepage'
+import { Homepage } from './containers/Homepage'
 import { Navbar } from './containers/Navbar'
 import { About } from './containers/About'
+import RestaurantPage from './containers/RestaurantPage'
 
-class App extends Component {
-  componentDidMount() {
-    this.props.fetchRestaurants()
-  }
-
+export default class App extends Component {
   render() {
     return (
       <Router>
-        <div>
+        <div className='app-container'>
          <Navbar />
          <Route exact path="/" component={Homepage}/>
-         <Route path="/pizza-reviews" component={RestaurantList}/>
+         <Route path="/pizza-reviews" component={RestaurantPage}/>
          <Route path="/about" component={About} />
         </div>
       </Router>
-
     );
   }
 }
-
-const mapDispatchToProps = dispatch => {
-  return bindActionCreators({
-    fetchRestaurants,
-  }, dispatch);
-};
-
-export default connect(null, mapDispatchToProps)(App);
