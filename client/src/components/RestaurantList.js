f66115f111ac3d147ad8ca6cfe7edf96fb703880
github username <yosa.yon@gmail.com>
2018-07-13 15:44:51 -0400

    added key to displayRestaurant

--- client/src/components/RestaurantList.js
+++ client/src/components/RestaurantList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Restaurant } from './Restaurant'
 
 export const RestaurantList = ({ restaurants }) => {
-  const displayRestaurant = restaurants.map((restaurant, index) => <Restaurant restaurant={restaurant} key={index}/>)
+  let displayRestaurant = restaurants.map((restaurant, index) => <Restaurant restaurant={restaurant} key={index}/>)
   return (
   <div>
     <h1>Reviews</h1>
