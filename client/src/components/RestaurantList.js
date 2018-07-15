f1c1118c3cfd35f22ff25424b67ce9ad1bd34a7e
github username <yosa.yon@gmail.com>
2018-07-14 23:27:13 -0400

    added table for all restaurants

--- client/src/components/RestaurantList.js
+++ client/src/components/RestaurantList.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import { Restaurant } from './Restaurant'
+import { NavLink } from 'react-router-dom'
 
 export const RestaurantList = ({ restaurants }) => {
   let displayRestaurant = restaurants.map((restaurant, index) => <Restaurant restaurant={restaurant} key={index}/>)
+  let length = displayRestaurant.length
   return (
-  <div>
-    <h1>Reviews</h1>
-    {displayRestaurant}
+  <div className='restaurant-list-container'>
+    <table>
+     <tbody>
+      <tr>
+        <th>Name:</th>
+        <th>Rating:</th>
+        <th>Video Review:</th>
+        <th>Address:</th>
+        <th>City:</th>
+        <th>State:</th>
+        <th>Zip code:</th>
+        <th>Phone:</th>
+        <th>Description:</th>
+      </tr>
+        {displayRestaurant}
+     </tbody>
+    </table>
   </div>
   )
 }
