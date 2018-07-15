1833b0c46eb24f81075a00464b23970c2eb194d6
github username <yosa.yon@gmail.com>
2018-07-14 23:22:44 -0400

    added lat and lng

--- app/admin/restaurants.rb
+++ app/admin/restaurants.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Restaurant do
-  permit_params :name, :rating, :address, :city, :state, :zipcode, :phone_number, :restaurant_url, :video_review_url, :description
+  permit_params :name, :rating, :address, :city, :state, :zipcode, :phone_number, :restaurant_url, :video_review_url, :description, :lat, :lng
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
