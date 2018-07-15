0d8a0033d6f10d9e021ef0d5a22a533275650011
github username <yosa.yon@gmail.com>
2018-07-14 23:26:21 -0400

    edited the way data displays within table

--- client/src/components/Restaurant.js
+++ client/src/components/Restaurant.js
@@ -3,13 +3,16 @@ import React from 'react';
 export const Restaurant = (props) => {
   const {name, rating, address, city, state, zipcode, phone_number, restaurant_url, video_review_url, description} = props.restaurant
   return (
-    <div>
-      <h3><a href={restaurant_url}>{name}</a></h3>
-      <p>Rated: {rating}</p>
-      <p>Description: {description}</p>
-      <p>{address} {city}, {state} {zipcode}</p>
-      <p>{phone_number}</p>
-      <p><a href={video_review_url}>Video Review</a></p>
-    </div>
+    <tr>
+      <td><a href={restaurant_url}>{name}</a></td>
+      <td><div>{rating}</div></td>
+      <td><div><a href={video_review_url}>Video Review</a></div></td>
+      <td><div>{address}</div></td>
+      <td><div>{city}</div></td>
+      <td><div>{state}</div></td>
+      <td><div>{zipcode}</div></td>
+      <td><div>{phone_number}</div></td>
+      <td><div>{description}</div></td>
+    </tr>
   )
 }
