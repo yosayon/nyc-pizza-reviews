362b87714a8f2330a4d7b3d9ec6701a38890b0db
github username <yosa.yon@gmail.com>
2018-07-14 23:29:08 -0400

    removed view maps from NavBar

--- client/src/containers/Navbar.js
+++ client/src/containers/Navbar.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom'
 import logo from '../pizza-logo.jpeg'
+import lineImage from '../line.png'
 
 export const Navbar = () => {
     return(
-      <div id='navbar-container'>
-
+      <div id='navbar-container-wrapper'>
+       <div id='navbar-container'>
          <NavLink className='nav-link' to='/pizza-reviews'> Reviews </NavLink>
          <NavLink className='pizza-logo' to='/'><img alt='logo' src={logo}/></NavLink>
          <NavLink className='nav-link' to='/about'> About </NavLink>
-
+       </div>
+       <div className='nav-bar-container-child-div'>
+        <img alt='line' src={lineImage}/>
+       </div>
       </div>
     )
   }
