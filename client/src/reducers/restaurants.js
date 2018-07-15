12b8a837e45b2e01356a94b173b7cc75fd767753
github username <yosa.yon@gmail.com>
2018-07-15 19:18:10 -0400

    added sortKeys into state

--- client/src/reducers/restaurants.js
+++ client/src/reducers/restaurants.js
@@ -1,10 +1,11 @@
-export default function restaurantReducer(state = {all: [], loading: false, sort: 'all' }, action){
+export default function restaurantReducer(state = {all: [], loading: false, sortKeys: [], sort: 'all' }, action){
   switch(action.type){
     case 'LOAD_RESTAURANTS':
     console.log("Loading.....")
       return {...state, loading: true }
     case 'ADD_RESTAURANTS':
-      return {...state, all: action.restaurants, loading: false }
+      let keys = Object.keys(action.restaurants[0]).filter(key => key !== 'id' && key !== 'lat' && key !== 'lng' && key !== 'restaurant_url')
+      return {...state, all: action.restaurants, loading: false, sortKeys: keys }
     case 'UPDATE_SORT_OPTION':
       return {...state, loading: false, sort: action.sort}
     default:
