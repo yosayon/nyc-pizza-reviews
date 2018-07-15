cfd445672bbb12b1dfbf5737a251983f0648ca36
github username <yosa.yon@gmail.com>
2018-07-15 19:18:55 -0400

    added additional sort actions

--- client/src/selectors/index.js
+++ client/src/selectors/index.js
@@ -15,12 +15,38 @@ export const getSortedRestaurants = createSelector(
           else if(a.name < b.name){return -1}
           else {return 0}
         })
-      case 'highest rated':
+      case 'rating':
         return restaurants.sort((a,b) => {
           if(b.rating > a.rating){return 1}
           else if(b.rating < a.rating){return -1}
           else {return 0}
       })
+      case 'address':
+        return restaurants.sort((a,b) => {
+          if(a.address > b.address){return 1}
+          else if(a.rating < b.rating){return -1}
+          else {return 0}
+        })
+      case 'city':
+        return restaurants.sort((a,b) => {
+          if(a.city > b.city){return 1}
+          else if(a.city < b.city){return -1}
+          else {return 0}
+        })
+      case 'zipcode':
+        return restaurants.sort((a,b) => {
+          if(a.zipcode > b.zipcode){return 1}
+          else if(a.zipcode < b.zipcode){return -1}
+          else {return 0}
+      })
+      case 'description':
+        return restaurants.sort((a, b) => {
+          if(a.description === "") return 1;
+          if(b.description === "") return -1;
+          if(a.description === b.description) return 0;
+          return a.description < b.description ? -1 : 1;
+        });
+
       default:
         return restaurants;
     }
