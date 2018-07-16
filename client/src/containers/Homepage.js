144a36e672becb935c38873783052921811c9c5f
github username <yosa.yon@gmail.com>
2018-07-16 19:34:58 -0400

    added search component, added searchText and restaurants to state, added componentDidUpdate, added onChange Text

--- client/src/containers/Homepage.js
+++ client/src/containers/Homepage.js
@@ -5,12 +5,14 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { fetchRestaurants, updateSortOption } from '../actions/restaurantActions'
 import { getSortedRestaurants } from '../selectors/index'
-import RestaurantList from '../components/RestaurantList'
-
+import RestaurantPage from './RestaurantPage'
+import { Search } from '../components/Search'
 
 export class Homepage extends Component{
   state={
-    currentSort: 'all'
+    currentSort: 'all',
+    searchText: '',
+    restaurants: []
   }
 
   componentDidMount(){
@@ -19,19 +21,34 @@ export class Homepage extends Component{
     }
   }
 
+  componentDidUpdate(){
+   if(this.state.restaurants[0] === undefined){this.setState({...this.state, restaurants: this.props.all})}
+  }
+
   handleSortChange = e => {
     e.preventDefault()
     this.props.updateSortOption(e.target.value)
     this.setState({ currentSort: e.target.value})
    }
 
+  onChangeText = e => {
+    let restaurants = this.props.all.filter(restaurant => {
+      return restaurant.name.toLowerCase().search(e.target.value.toLowerCase()) !== -1;
+    })
+    this.setState({
+      searchText: e.target.value,
+      restaurants: restaurants
+    })
+  }
+
   render(){
     return(
     <div id='homepage-container'>
         <div className='one-bite-message'>ONE BITE EVERYBODY KNOWS THE RULES - Dave Portnoy, El Presidente</div>
         <div className='one-bite-message'>Visit <NavLink to='http://www.barstoolsports.com'>barstoolsports</NavLink></div>
-        <MapContainer restaurants={this.props.all}/>
-        <RestaurantList restaurants={this.props.all} handleSortChange={this.handleSortChange} sortKeys={this.props.sortKeys}/>
+        <MapContainer restaurants={this.state.restaurants}/>
+        <Search onChangeText={this.onChangeText} searchText={this.state.searchText}/>
+        <RestaurantPage onChangeText={this.onChangeText} restaurants={this.state.restaurants} handleSortChange={this.handleSortChange} sortKeys={this.props.sortKeys}/>
     </div>
     )
   }
