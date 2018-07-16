d97fe7e0981d9e710c0c434e56e39fc44a8cd457
github username <yosa.yon@gmail.com>
2018-07-16 19:35:40 -0400

    initially had state in here but making it stateless

--- client/src/containers/RestaurantPage.js
+++ client/src/containers/RestaurantPage.js
@@ -1,53 +1,17 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { fetchRestaurants, updateSortOption } from '../actions/restaurantActions';
-import { getSortedRestaurants } from '../selectors/index'
 import RestaurantList from '../components/RestaurantList'
-import MapContainer from '../containers/MapContainer'
 
 
-class RestaurantPage extends Component{
-
-  state={
-    currentSort: 'all'
-  }
-
-  componentDidMount(){
-    if(this.props.all.length === 0){
-      this.props.fetchRestaurants();
-    }
-  }
-
-  handleSortChange = e => {
-    e.preventDefault()
-    this.props.updateSortOption(e.target.value)
-    this.setState({ currentSort: e.target.value})
-   }
-
+export default class RestaurantPage extends Component{
   render(){
-    return(
-      <div className='restaurant-page-container'>
-        <MapContainer restaurants={this.props.all}/>
-
-        <RestaurantList restaurants={this.props.all} handleSortChange={this.handleSortChange} sortKeys={this.props.sortKeys}/>
-      </div>
+    return (
+    <div className='restaurant-page-container'>
+      <RestaurantList
+        restaurants={this.props.restaurants}
+        handleSortChange={this.props.handleSortChange}
+        sortKeys={this.props.sortKeys}
+      />
+    </div>
     )
   }
 }
-
-const mapDispatchToProps = dispatch => {
-  return bindActionCreators({
-    fetchRestaurants,
-    updateSortOption,
-  }, dispatch);
-};
-
-const mapStateToProps = state => {
-  return ({
-    all: getSortedRestaurants(state),
-    sortKeys: state.restaurants.sortKeys
-  })
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(RestaurantPage);
