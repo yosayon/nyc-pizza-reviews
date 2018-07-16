ea1fea80ee69847171f1d54e978c7d1600364c08
github username <yosa.yon@gmail.com>
2018-07-16 19:36:46 -0400

    edited message when loading restaurants

--- client/src/reducers/restaurants.js
+++ client/src/reducers/restaurants.js
@@ -1,7 +1,7 @@
 export default function restaurantReducer(state = {all: [], loading: false, sortKeys: [], sort: 'all' }, action){
   switch(action.type){
     case 'LOAD_RESTAURANTS':
-    console.log("Loading.....")
+    console.log("Loading Restaurants...")
       return {...state, loading: true }
     case 'ADD_RESTAURANTS':
       let keys = Object.keys(action.restaurants[0]).filter(key => key !== 'id' && key !== 'lat' && key !== 'lng' && key !== 'restaurant_url')
