95a75c5af335044c89eadf0fa65bd68795216e8d
github username <yosa.yon@gmail.com>
2018-07-16 19:33:59 -0400

    created search component for homepage

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import searchIcon from '../search-icon.png'
 
-export const Search = () => {
+export const Search = ({onChangeText, searchText}) => {
   return(
     <div className='search-container'>
-      <img className='search-icon' alt='search icon' src={searchIcon}/>
-      <input className='search-box' type='text'/>
+      <img
+        className='search-icon'
+        alt='search icon'
+        src={searchIcon}
+      />
+      <input
+        className='search-box'
+        type='text'
+        onChange={onChangeText}
+        value={searchText}
+        placeholder='Search Review'
+      />
     </div>
   )
 }
