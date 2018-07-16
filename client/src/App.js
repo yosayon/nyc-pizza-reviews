e58511aecf8a993ea7d2049216cf6bdb67da207f
github username <yosa.yon@gmail.com>
2018-07-16 16:44:55 -0400

    removed route to view map

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-import { Homepage } from './containers/Homepage'
+import  Homepage from './containers/Homepage'
 import { Navbar } from './containers/Navbar'
 import { About } from './containers/About'
 import RestaurantPage from './containers/RestaurantPage'
-import MapContainer from './containers/MapContainer'
 
 export default class App extends Component {
   render() {
@@ -14,9 +13,7 @@ export default class App extends Component {
         <div className='app-container'>
          <Navbar />
          <Route exact path="/" component={Homepage}/>
-         <Route path="/pizza-reviews" component={RestaurantPage}/>
          <Route path="/about" component={About} />
-         <Route path='/view-map' component={MapContainer}/>
         </div>
       </Router>
     );
