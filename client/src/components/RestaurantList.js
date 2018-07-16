50a378f007f1b266bc49cfba9de50f526a7a8404
github username <yosa.yon@gmail.com>
2018-07-16 16:45:15 -0400

    added Search component

--- client/src/components/RestaurantList.js
+++ client/src/components/RestaurantList.js
@@ -1,19 +1,22 @@
 import React from 'react'
 import { Restaurant } from './Restaurant'
 import { SortButton } from './SortButton'
+import searchIcon from '../search-icon.png'
+import { Search } from '../components/Search'
 
 const RestaurantList = ({ restaurants, handleSortChange, sortKeys}) => {
   let displayRestaurant = restaurants.map(restaurant => <Restaurant restaurant={restaurant} key={restaurant.id} sortKeys={sortKeys}/>)
-  let displayButton = sortKeys.map(key => <th><SortButton text={key} handleSortChange={handleSortChange}/></th>)
+  let displayButton = sortKeys.map(key => <th key={key}><SortButton text={key} handleSortChange={handleSortChange}/></th>)
   return (
   <div className='restaurant-list-container'>
+    <Search />
     <table>
-     <tbody>
+      <tbody>
       <tr>
         {displayButton}
       </tr>
         {displayRestaurant}
-     </tbody>
+      </tbody>
     </table>
   </div>
   )
