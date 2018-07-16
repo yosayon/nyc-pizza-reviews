569605c522d0f60f08207f0ce1ce405c6e7b5f45
github username <yosa.yon@gmail.com>
2018-07-16 16:45:46 -0400

    added them removed Search component. placing this inside restaurantlist instead

--- client/src/containers/Homepage.js
+++ client/src/containers/Homepage.js
@@ -1,15 +1,54 @@
-import React from 'react';
-import background from '../pizza-background.png'
+import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom'
+import MapContainer from './MapContainer'
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchRestaurants, updateSortOption } from '../actions/restaurantActions'
+import { getSortedRestaurants } from '../selectors/index'
+import RestaurantList from '../components/RestaurantList'
 
-export const Homepage = () => {
-  return(
-    <div id='background-container'>
-      <img alt='background' className='pizza-background' src={background}/>
-      <div className='overlay'>
+
+export class Homepage extends Component{
+  state={
+    currentSort: 'all'
+  }
+
+  componentDidMount(){
+    if(this.props.all.length === 0){
+      this.props.fetchRestaurants();
+    }
+  }
+
+  handleSortChange = e => {
+    e.preventDefault()
+    this.props.updateSortOption(e.target.value)
+    this.setState({ currentSort: e.target.value})
+   }
+
+  render(){
+    return(
+    <div id='homepage-container'>
         <div className='one-bite-message'>ONE BITE EVERYBODY KNOWS THE RULES - Dave Portnoy, El Presidente</div>
         <div className='one-bite-message'>Visit <NavLink to='http://www.barstoolsports.com'>barstoolsports</NavLink></div>
-      </div>
+        <MapContainer restaurants={this.props.all}/>
+        <RestaurantList restaurants={this.props.all} handleSortChange={this.handleSortChange} sortKeys={this.props.sortKeys}/>
     </div>
-  )
+    )
+  }
 }
+
+const mapDispatchToProps = dispatch => {
+  return bindActionCreators({
+    fetchRestaurants,
+    updateSortOption,
+  }, dispatch);
+};
+
+const mapStateToProps = state => {
+  return ({
+    all: getSortedRestaurants(state),
+    sortKeys: state.restaurants.sortKeys
+  })
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Homepage);
