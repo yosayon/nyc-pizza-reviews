8cea16af6a18875ebc2fe767f6623293bb0770d4
github username <yosa.yon@gmail.com>
2018-07-18 17:07:51 -0400

    users are now able to filter and sort the data

--- client/src/containers/Homepage.js
+++ client/src/containers/Homepage.js
@@ -3,43 +3,51 @@ import { NavLink } from 'react-router-dom'
 import MapContainer from './MapContainer'
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { fetchRestaurants, updateSortOption } from '../actions/restaurantActions'
+import { fetchRestaurants, updateSortOption} from '../actions/restaurantActions'
 import { getSortedRestaurants } from '../selectors/index'
 import RestaurantPage from './RestaurantPage'
 import { Search } from '../components/Search'
+
 
 export class Homepage extends Component{
   state={
     currentSort: 'all',
     searchText: '',
-    restaurants: []
+    restaurants: [],
+    active: false
+  }
+  componentDidMount(){
+    this.props.fetchRestaurants();
   }
 
-  componentDidMount(){
-    if(this.props.all.length === 0){
-      this.props.fetchRestaurants();
+  componentDidUpdate(prevProps, prevState){
+   if(this.state.active === false){
+      this.setState({
+        ...this.state,
+        restaurants: this.props.all,
+        active: true
+      })
     }
   }
-
-  componentDidUpdate(){
-   if(this.state.restaurants[0] === undefined){this.setState({...this.state, restaurants: this.props.all})}
-  }
-
   handleSortChange = e => {
-    e.preventDefault()
-    this.props.updateSortOption(e.target.value)
-    this.setState({ currentSort: e.target.value})
-   }
+   e.preventDefault()
+   this.props.updateSortOption(e.target.value, this.state.restaurants)
+   this.setState({
+     ...this.state,
+     currentSort: e.target.value,
+   })
+  }
 
   onChangeText = e => {
     let restaurants = this.props.all.filter(restaurant => {
-      return restaurant.name.toLowerCase().search(e.target.value.toLowerCase()) !== -1;
-    })
+        return restaurant.name.toLowerCase().search(e.target.value.toLowerCase()) !== -1;
+      })
     this.setState({
+      ...this.state,
       searchText: e.target.value,
       restaurants: restaurants
     })
-  }
+   }
 
   render(){
     return(
@@ -47,7 +55,7 @@ export class Homepage extends Component{
         <div className='one-bite-message'>ONE BITE EVERYBODY KNOWS THE RULES - Dave Portnoy, El Presidente</div>
         <div className='one-bite-message'>Visit <NavLink to='http://www.barstoolsports.com'>barstoolsports</NavLink></div>
         <MapContainer restaurants={this.state.restaurants}/>
-        <Search onChangeText={this.onChangeText} searchText={this.state.searchText}/>
+        <Search onChangeText={this.onChangeText} searchText={this.state.searchText} count={this.state.restaurants.length}/>
         <RestaurantPage onChangeText={this.onChangeText} restaurants={this.state.restaurants} handleSortChange={this.handleSortChange} sortKeys={this.props.sortKeys}/>
     </div>
     )
@@ -63,8 +71,9 @@ const mapDispatchToProps = dispatch => {
 
 const mapStateToProps = state => {
   return ({
-    all: getSortedRestaurants(state),
-    sortKeys: state.restaurants.sortKeys
+    all: state.restaurants.all,
+    sortKeys: state.restaurants.sortKeys,
+    sortedRestaurants: getSortedRestaurants(state)
   })
 }
 
