8283586953d939571c58dbc53853165c8301ec65
github username <yosa.yon@gmail.com>
2018-07-18 17:08:36 -0400

    added sortedRestaurants

--- client/src/reducers/restaurants.js
+++ client/src/reducers/restaurants.js
@@ -1,13 +1,15 @@
-export default function restaurantReducer(state = {all: [], loading: false, sortKeys: [], sort: 'all' }, action){
+export default function restaurantReducer(state = {all: [], loading: false, sortKeys: [], sort: 'all', sortedRestaurants: []}, action){
   switch(action.type){
     case 'LOAD_RESTAURANTS':
     console.log("Loading Restaurants...")
       return {...state, loading: true }
     case 'ADD_RESTAURANTS':
       let keys = Object.keys(action.restaurants[0]).filter(key => key !== 'id' && key !== 'lat' && key !== 'lng' && key !== 'restaurant_url')
-      return {...state, all: action.restaurants, loading: false, sortKeys: keys }
+      return {...state, all: action.restaurants, loading: false, sortKeys: keys, sortedRestaurants: action.restaurants }
     case 'UPDATE_SORT_OPTION':
       return {...state, loading: false, sort: action.sort}
+    case 'UPDATE_SORTED_RESTAURANTS':
+      return {...state, loading: false, sortedRestaurants: action.sortedRestaurants}
     default:
       return state;
   }
