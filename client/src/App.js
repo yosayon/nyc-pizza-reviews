87da9423a1fd49b19cafd8b684fad42525a58128
github username <yosa.yon@gmail.com>
2018-07-21 12:55:45 -0400

    added vote route

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route} from 'react-router-dom'
 import  Homepage from './containers/Homepage'
 import { Navbar } from './containers/Navbar'
 import { About } from './containers/About'
-import RestaurantPage from './containers/RestaurantPage'
+import { VotePage } from './containers/VotePage'
 
 export default class App extends Component {
   render() {
     return (
-      <Router>
+
+        <Router>
         <div className='app-container'>
          <Navbar />
          <Route exact path="/" component={Homepage}/>
          <Route path="/about" component={About} />
+         <Route path="/vote" component={VotePage} />
         </div>
-      </Router>
+        </Router>
+
     );
   }
 }
