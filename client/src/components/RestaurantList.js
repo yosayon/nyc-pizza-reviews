873937804ef972ac38e0baf882933be302b38b17
github username <yosa.yon@gmail.com>
2018-07-21 12:56:12 -0400

    removed unused import

--- client/src/components/RestaurantList.js
+++ client/src/components/RestaurantList.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import { Restaurant } from './Restaurant'
 import { SortButton } from './SortButton'
-import searchIcon from '../search-icon.png'
-import { Search } from '../components/Search'
 
 const RestaurantList = ({ restaurants, handleSortChange, sortKeys}) => {
   let displayRestaurant = restaurants.map(restaurant => <Restaurant restaurant={restaurant} key={restaurant.id} sortKeys={sortKeys}/>)
