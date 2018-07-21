d6a2ba4c196aa6185930d3c54f20679bb74ffb66
github username <yosa.yon@gmail.com>
2018-07-21 19:32:00 -0400

    added index for all actions

---
+++ client/src/actions/index.js
@@ -0,0 +1,6 @@
+import { createRecommendation } from './recommendationActions'
+import { fetchRestaurants, updateSortOption } from './restaurantActions'
+
+export { createRecommendation }
+export { fetchRestaurants }
+export { updateSortOption }
