ab86d391d068211b61ca810da7fb4630b11a2f3e
github username <yosa.yon@gmail.com>
2018-07-21 19:35:19 -0400

    added propTypes and passed props from parent

--- client/src/components/VoteForm.js
+++ client/src/components/VoteForm.js
@@ -1,27 +1,57 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 
-export const VoteForm = ({onHandleFormSubmit}) => {
+export const VoteForm = ({onHandleFormSubmit, name, address, city, onChangeText, formErrors, errorClass}) => {
   return(
     <form onSubmit={onHandleFormSubmit}>
-      <div>Name: </div>
-      <input name="name" type='text' value/>
+      <div>Name: {formErrors.name} </div>
+      <input
+        name="name"
+        type='text'
+        value={name}
+        onChange={onChangeText}
+        className={`form-group ${errorClass(formErrors.name)}`}
+      />
       <br/>
-      <div>Address: </div>
-      <input name='address' type='text'/>
+      <div>Address: {formErrors.address} </div>
+      <input
+        name='address'
+        type='text'
+        value={address}
+        onChange={onChangeText}
+        className={`form-group ${errorClass(formErrors.name)}`}
+      />
       <br />
-      <div>City: </div>
-      <input name='city' type='text'/>
+      <div>City: {formErrors.city}</div>
+      <input
+        name='city'
+        type='text'
+        value={city}
+        onChange={onChangeText}
+        className={`form-group ${errorClass(formErrors.name)}`}
+      />
       <br />
       <div>State: </div>
       <select>
-        <option name='new york' value='new york'>New York</option>
+        <option
+          name='new york'
+          value='new york'>
+            New York
+        </option>
       </select>
       <br />
-      <input type="submit" value='Submit'/>
+      <input
+        type="submit"
+        value='Submit'
+      />
     </form>
   )
 }
 
 VoteForm.propTypes={
-
+  onHandleFormSubmit: PropTypes.func,
+  name: PropTypes.string,
+  address: PropTypes.string,
+  city: PropTypes.string,
+  onChangeText: PropTypes.func
 }
