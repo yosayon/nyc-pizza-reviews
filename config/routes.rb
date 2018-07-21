a84be6f42b3d2554b9f8fab587f5593ddc8f8a26
github username <yosa.yon@gmail.com>
2018-07-21 12:59:07 -0400

    added routes for create and index

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   scope '/api' do
   resources :restaurants, :only => [:index]
+  resources :recommendations, :only => [:create, :index]
   end
 end
