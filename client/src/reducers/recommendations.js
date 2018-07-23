a3a73cf24a69f2e07b76779fdc68ebf5ab6e4e94
github username <yosa.yon@gmail.com>
2018-07-23 19:59:57 -0400

    added two new actions to recommendationsReducer

--- client/src/reducers/recommendations.js
+++ client/src/reducers/recommendations.js
@@ -1,14 +1,15 @@
-export default function recommendationReducer(state = {
-  all: [],
-  loading: false
-  },
-  action){
+export default function recommendationReducer(state = {all: [],loading: false},action){
   switch(action.type){
     case 'LOADING':
     console.log("Loading...")
       return {...state, loading: true}
     case 'ADD_NEW_RECOMMENDATION':
-      return {...state.all.push(action.rec), loading: false, }
+      console.log("Adding Recommendation that was just created...", action.recommendation)
+      return {all: state.all.push(action.recommendation), loading: false }
+    case 'ADD_RECOMMENDATIONS':
+      console.log("Adding Recommendations From API....")
+      let recs = action.recommendations
+      return {all: recs, loading: false}
     default:
       return state;
   }
