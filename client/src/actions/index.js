f50d2343d6a08f6a33ae6a26a7393630a3599ccc
github username <yosa.yon@gmail.com>
2018-07-23 19:57:17 -0400

    no changes here

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,6 +1,9 @@
-import { createRecommendation } from './recommendationActions'
+import { createRecommendation, fetchRecommendations } from './recommendationActions'
+import { addRecommendation } from './recommendationActions'
 import { fetchRestaurants, updateSortOption } from './restaurantActions'
 
 export { createRecommendation }
+export { fetchRecommendations }
+export { addRecommendation }
 export { fetchRestaurants }
 export { updateSortOption }
