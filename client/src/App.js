fd69687a993b22a147db7cecb6ed1e3edce3e279
github username <yosa.yon@gmail.com>
2018-07-23 19:56:50 -0400

    removed routes from inside App component and moved it all into RouteContainer

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route} from 'react-router-dom'
-import  Homepage from './containers/Homepage'
-import { Navbar } from './containers/Navbar'
-import { About } from './containers/About'
-import { VotePage } from './containers/VotePage'
+import { BrowserRouter as Router} from 'react-router-dom'
+import RouteContainer from './RouteContainer'
 
 export default class App extends Component {
   render() {
     return (
-
         <Router>
-        <div className='app-container'>
-         <Navbar />
-         <Route exact path="/" component={Homepage}/>
-         <Route path="/about" component={About} />
-         <Route path="/vote" component={VotePage} />
-        </div>
+          <RouteContainer />
         </Router>
-
     );
   }
 }
