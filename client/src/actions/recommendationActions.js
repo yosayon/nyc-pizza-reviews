afc7dc2d09c786e5189b16cc37ecd71a332c8303
github username <yosa.yon@gmail.com>
2018-07-23 19:57:54 -0400

    added isomorphic fetch, added createRecommendation and fetchRecommendation actioncreators

--- client/src/actions/recommendationActions.js
+++ client/src/actions/recommendationActions.js
@@ -1,11 +1,41 @@
 import fetch from 'isomorphic-fetch'
 
+// export function success(response){
+//   return dispatch => {
+//     dispatch({type: 'LOADING'})
+//     dispatch({type: 'ADD_NEW_RECOMMENDATION', recommendation: response.json()})
+//   }
+// }
+//
+// export function failure(error){
+//   return dispatch => {
+//     dispatch({type: 'CREATE_REC_FAILED', error})
+//   }
+// }
+
 export function createRecommendation(recommendation){
- let body = JSON.stringify({recommendation})
+  return function(dispatch){
+      dispatch({type: 'LOADING'})
+      fetch('/api/recommendations', {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json','Accept': 'application/json'},
+      body: recommendation
+    })
+    .then(response => response.json())
+    .then(recommendation => {
+      dispatch({type: 'ADD_NEW_RECOMMENDATION', recommendation: recommendation})
+    })
+  }
+}
+
+export function fetchRecommendations(){
+  console.log("Fetching recommendations...")
   return function(dispatch){
     dispatch({type: 'LOADING'})
-    return fetch('/api/recommendations', {method: 'POST',headers: {'Content-Type': 'application/json','Accept': 'application/json'},body: body})
-    .then(res => res.json())
-    .then (rec => {dispatch({ type: 'ADD_NEW_RECOMMENDATION', recommendation: rec})})
+    return fetch('/api/recommendations')
+    .then(response => response.json())
+    .then(recommendations => {
+      dispatch({type: 'ADD_RECOMMENDATIONS', recommendations: recommendations})
+    })
   }
 }
