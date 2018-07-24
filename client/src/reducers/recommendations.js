addb12f8f10c52f26bdbb6b6f3e5589ee07435dd
github username <yosa.yon@gmail.com>
2018-07-24 00:12:38 -0400

    fixed state

--- client/src/reducers/recommendations.js
+++ client/src/reducers/recommendations.js
@@ -5,11 +5,19 @@ export default function recommendationReducer(state = {all: [],loading: false},a
       return {...state, loading: true}
     case 'ADD_NEW_RECOMMENDATION':
       console.log("Adding Recommendation that was just created...", action.recommendation)
-      return {all: state.all.push(action.recommendation), loading: false }
+      let rec = action.recommendation
+      return {all: [...state.all, rec], loading: false }
     case 'ADD_RECOMMENDATIONS':
       console.log("Adding Recommendations From API....")
-      let recs = action.recommendations
+      let recs = action.recommendations.recommendations
       return {all: recs, loading: false}
+    case 'UPVOTE':
+      console.log("Voting up!")
+      let recommendation = [action.recommendation]
+      console.log(recommendation)
+      let recommendations = state.all.filter(rec => rec.id !== action.recommendation.id)
+      let all = recommendation.concat(recommendations)
+      return {all: all, loading: false}
     default:
       return state;
   }
