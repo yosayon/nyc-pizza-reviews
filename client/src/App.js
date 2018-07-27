43a015d6ebfcc82c365399348959c6dfab227e2d
github username <yosa.yon@gmail.com>
2018-07-26 22:47:48 -0400

    added routes inside app

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
-import { BrowserRouter as Router} from 'react-router-dom'
-import RouteContainer from './containers/RouteContainer'
+import { BrowserRouter, Switch, Route} from 'react-router-dom'
+// import RouteContainer from './containers/RouteContainer'
+import { MockNavbar } from './components/MockNavbar'
+import { MockHomepageContainer} from './containers/MockHomepageContainer'
+import { About } from './components/About'
+import { VotePage } from './components/VotePage'
 
 export default class App extends Component {
   render() {
     return (
-        <Router>
-          <RouteContainer />
-        </Router>
+      <BrowserRouter>
+          <div className='app-container'>
+            <MockNavbar/>
+              <Switch>
+                  <Route exact path="/" component={MockHomepageContainer}/>
+                  <Route path="/vote" component={VotePage} />
+                  <Route path="/about" component={About} />
+              </Switch>
+          </div>
+      </BrowserRouter>
     );
   }
 }
