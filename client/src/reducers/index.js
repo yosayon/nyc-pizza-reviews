8ef3363c76e2f056b47a309ee83aff7af36241e9
github username <yosa.yon@gmail.com>
2018-07-27 23:25:37 -0400

    moved all reducers to respective Util folder

--- client/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import restaurants from './restaurants';
-import recommendations from './recommendations'
-
-export const rootReducer = combineReducers({
-  restaurants,
-  recommendations
-});
