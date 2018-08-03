5c57f93cf0abb7607980997c79354e4dbd6611e0
github username <yosa.yon@gmail.com>
2018-08-02 20:47:50 -0400

    no changes really made here, updated const to be class

--- client/src/1_Root/App.js
+++ client/src/1_Root/App.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../Style/App.css';
 import { Navbar } from '../Header/Navbar'
 import Main from './Main'
 
-const App = () =>
-  <div className='app-container'>
-    <Navbar/>
-    <Main />
-  </div>
+export class App extends Component{
+
+  render(){
+    return(
+      <div className='app-container'>
+        <Navbar />
+        <Main />
+      </div>
+    )
+  }
+}
 
 export default App
