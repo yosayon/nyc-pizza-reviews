0429175f02f42ed1b6dcc1151bf96c36a2061396
github username <yosa.yon@gmail.com>
2018-08-02 21:00:18 -0400

    added user validation

---
+++ app/models/user.rb
@@ -0,0 +1,4 @@
+class User < ApplicationRecord
+  validates :email, :presence => true, :uniqueness => true
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+end
