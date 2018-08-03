892194bfd139696d3c82d2d895a21c48cde9ea0e
github username <yosa.yon@gmail.com>
2018-08-02 20:56:02 -0400

    added usersReducer and added it to rootReducer

--- client/src/Util/reducers/index.js
+++ client/src/Util/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import restaurants from './restaurants';
 import recommendations from './recommendations'
+import users from './users'
 
 export const rootReducer = combineReducers({
   restaurants,
-  recommendations
+  recommendations,
+  users
 });
