dbd7413eedf569ec505b92f1db84931e5dc445c9
github username <yosa.yon@gmail.com>
2018-08-02 20:54:30 -0400

    added find_or_create_user and logout

--- client/src/Util/actions/index.js
+++ client/src/Util/actions/index.js
@@ -1,6 +1,7 @@
 import { createRecommendation, fetchRecommendations, upVote } from './recommendationActions'
 import { addRecommendation } from './recommendationActions'
 import { fetchRestaurants, updateSortOption } from './restaurantActions'
+import { find_or_create_user, logout } from './userActions'
 
 export { createRecommendation }
 export { fetchRecommendations }
@@ -8,3 +9,5 @@ export { upVote }
 export { addRecommendation }
 export { fetchRestaurants }
 export { updateSortOption }
+export { find_or_create_user }
+export { logout }
