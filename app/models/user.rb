e7fee0196af660549aaca168fab0a361ac8d0312
github username <yosa.yon@gmail.com>
2018-08-04 21:24:22 -0400

    added associated relationship. user has many comments, has_many votes and has_many recommendations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  has_many :comments
+  has_many :votes
+  has_many :recommendations
   validates :email, :presence => true, :uniqueness => true
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+
 end
