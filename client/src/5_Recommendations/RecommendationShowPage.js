c42ccc499d9c6ab96de5955c74a60823f2028906
github username <yosa.yon@gmail.com>
2018-08-04 21:42:26 -0400

    added show page for recommendation to show commments

--- client/src/5_Recommendations/RecommendationShowPage.js
+++ client/src/5_Recommendations/RecommendationShowPage.js
@@ -1,29 +1,22 @@
 import React from 'react';
-import voteButton from './images/pizza-logo1.png'
-import { Link } from 'react-router-dom';
+import { Recommendation } from './Recommendation'
 
 export const RecommendationShowPage = ({recommendations, onVote, match}) => {
   const id = Number(match.params.recommendationId)
   const recommendation = recommendations.find(rec => rec.id === id)
-  const handleOnClick = (e) => {
-    onVote(recommendation.id)
-  }
+
+  let comments = recommendation.comments.map(comment => comment.comment) || "No comments available"
+
   return(
-  <div className='recommendations-list-container'>
-    <div className='recommendation-container'>
-      <div className='vote-button'>
-        <img src={voteButton} alt='button' onClick={(e) => handleOnClick(e)} />
-        <span className='vote-text'>Click on me to vote</span>
-      </div>
-      <div className='rec-content'><h1>{recommendation.name}</h1></div>
-      <div className='rec-content'>{recommendation.address}</div>
-      <div className='rec-content'>{recommendation.city}</div>
-      <div className='rec-content'>{recommendation.state}</div>
-      <div className='rec-content'>Votes:{recommendation.votes}</div>
-      <div className='rec-comment'>
-        <Link to={`/recommendations/${recommendation.id}`}>view comments - {recommendation.comments === null ? 0 : recommendation.comments.length}</Link>
-      </div>
+    <div className='recommendations-list-container'>
+      <Recommendation recommendation={recommendation} onVote={onVote}/>
+      <div>
+        <form>
+          Enter Comment:
+          <input name="comments" type='text' />
+          </form>
+        </div>
+      {comments}
     </div>
-  </div>
     )
   }
