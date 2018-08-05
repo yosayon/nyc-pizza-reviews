7014f047cd3d0729593211eb32a02d6c8fa03d83
github username <yosa.yon@gmail.com>
2018-08-04 21:47:42 -0400

    added match to Recommendation component in RecommendationsList

--- client/src/5_Recommendations/RecommendationsList.js
+++ client/src/5_Recommendations/RecommendationsList.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Recommendation } from './Recommendation'
 
 export const RecommendationsList = ({match, recommendations, onVote}) => {
-  let displayRecommendations = recommendations.map(rec => <Recommendation onVote={onVote} recommendation={rec} key={rec.id}/>)
+  let displayRecommendations = recommendations.map(rec => <Recommendation match={match} onVote={onVote} recommendation={rec} key={rec.id}/>)
   return(
     <div className='recommendations-list-container'>
       {displayRecommendations}
