e8fb56dd2bd141ce4c0ab6f3021b2cde08a9aaa7
github username <yosa.yon@gmail.com>
2018-08-04 21:21:40 -0400

    reconfigured actions within controller so that it throws an http error code if the resource cannot be created

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -4,19 +4,27 @@ class RecommendationsController < ApiController
   end
 
   def create
-    recommendation = Recommendation.create(recommendation_params)
-    render json: recommendation
+    @recommendation = Recommendation.new(recommendation_params)
+    if !@recommendation.save
+      render :json => {:status => 422, :message => 'Recommendation was not created', :error => @recommendation.errors}
+    else
+      render :json => @recommendation
+    end
   end
 
   def update
-    recommendation = Recommendation.find(params['id'])
-    recommendation.increaseVote
-    render json: recommendation
+    @recommendation = Recommendation.find(params['id'])
+    @recommendation.votes.create(:user_id => params['user_id'], :recommendation_id => params['id'])
+    if !@recommendation
+      render :json => {status: 422, :message => 'Recommendation was not updated', :errors => @recommendation.errors}
+    else
+      render :json => @recommendation
+    end
   end
 
 private
 
   def recommendation_params
-    params.require(:recommendation).permit(:name, :address, :city, :state, :comments, :votes)
+    params.require(:recommendation).permit(:name, :address, :city, :state, :comments, :votes, :user_id)
   end
 end
